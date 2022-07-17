98c32c27dd43a98c3ccd4f9748fffe50c144cb49
Soner A <107695479+manwelith@users.noreply.github.com>
2022-07-17 16:08:06 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReturnDeger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int toplanandeger = toplamaYap(4, 5);
+
+            if (toplanandeger>10)
+            {
+                Console.WriteLine("Sonuç 10dan Büyüktür.");
+            }
+            else
+            {
+                Console.WriteLine("Sonuç 10dan Küçüktür.");
+            }*/
+            IsimSoyisim("Soner", "Arı");
+            
+
+
+            Console.ReadLine();
+        }
+        /*static int toplamaYap(int sayi1, int sayi2)
+        {
+            int sonuc = sayi1 + sayi2;
+            Console.WriteLine(sonuc);
+            return sonuc;
+        }*/
+
+        static string IsimSoyisim(string isim, string soyisim)
+        {
+            string toplam = isim  +" "+  soyisim;
+            Console.WriteLine(toplam);
+
+            return toplam;
+
+        }
+
+
+    }
+}
